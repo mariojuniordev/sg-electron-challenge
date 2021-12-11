5681acb4ae8fb34e44c0f535668c8c4ee17a2330
mariojuniordev <mario_10gt@hotmail.com>
2021-12-11 17:32:28 -0300

    JS conception, HTML update

--- renderer.js
+++ renderer.js
@@ -1,6 +1,57 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const api = {
+  key: "e0d21f9c4c4c1e7209069a7f555da34b",
+  url: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchInput = document.querySelector('.search-input');
+searchInput.addEventListener('keypress', setQuery);
+
+// setQuery receives an EventListener as parameter and executes the
+// getResults function (receiving the searchInput.value as parameter 
+// if the 'keypress' event.keyCode equals 13 (which is the ENTER key code)
+function setQuery(event) {
+  if (event.keyCode == 13) {
+    getResults(searchInput.value);
+  }
+}
+
+// getResults consumes the API, converts the responses to the JSON format
+// and then executes the showResults function
+function getResults(query) {
+  fetch(`${api.url}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(showResults);
+}
+
+// showResults shows the weather, city name and country name results on the screen
+function showResults (weather) {
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let today = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = buildDate(today);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+  let climate = document.querySelector('.current .weather');
+  climate.innerText = weather.weather[0].main;
+
+  let minMax = document.querySelector('.min-max');
+  minMax.innerText = `Min. ${Math.round(weather.main.temp_min)}°c / Max. ${Math.round(weather.main.temp_max)}°c`;
+}
+
+// buildDate returns the date in format "week day, date(number), month, year"
+function buildDate(d) {
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day}, ${date} ${month} ${year}`;
+}
