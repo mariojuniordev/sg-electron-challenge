2d86bb541b5c4dd4386f886a3ee915a6a2218703
mariojuniordev <mario_10gt@hotmail.com>
2021-12-12 12:36:55 -0300

    Style Update and Footer

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1280,
-    height: 720,
+    height: 900,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
